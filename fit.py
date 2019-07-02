98bd1ff1dfb8fc648b38211508970558faa07564
Siddhartha Dhiman <sdhiman@buffalo.edu>
2019-07-02 16:48:21 -0400

    Minor changes

--- fit.py
+++ fit.py
@@ -3,25 +3,30 @@ import numpy as np
 import scipy as scp
 import nibabel as nib
 import os
+import designerfunc as df
 
 print('Import successful!')
 
 # Generate paths to files
 niiPath = '/Users/sid/Downloads/nii_test/DWI/DKI_BIPOLAR_2mm_64dir_62slices_20190206093716_18.nii'
-(path, file)  = os.path.split(inFile)
+dwi = df.loadnii(niiPath)
+
+
+
+(path, file)  = os.path.split(niiPath)
 fName = os.path.splitext(file)[0]
 bvalPath = os.path.join(path,fName + '.bval')
 bvecPath = os.path.join(path,fName + '.bvec')
 
 # Read Files
-hdr = nib.load(inFile)
-dwi = hdr.get_fdata()
+hdr = nib.load(niiPath)
+dwi = hdr.get_fdata
+print("Loaded into memory: %s" % (niiPath))
 
 bvals = np.rint(np.loadtxt(bvalPath))
 bvecs = np.loadtxt(bvecPath)
-grad = np.concatenate([np.transpose(bvecs),bvals],1)
+grad = np.c_[np.transpose(bvecs),bvals]
 
 # Tolerance Level: Smallest acceptable diffusion and kurtosis value. Values above and below this are non-zeros and zeros
 # respectively
 minparam = 1e-8
-
