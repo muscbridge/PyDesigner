Fix brain masking

designer/pydesigner.py
@@ -160,11 +160,11 @@ parser.add_argument('--kcumulants', action='store_true', default=False,
 parser.add_argument('--mask', action='store_true', default=False,
                     help='Compute a brain mask prior to tensor fitting '
                     'to strip skull and improve efficiency. Use '
-                     '--brainthresh to specify a threshold manually.')
-parser.add_argument('--maskthresh', metavar='< fractional intensity '
+                     '--maskthr to specify a threshold manually.')
+parser.add_argument('--maskthr', metavar='< fractional intensity '
                                              ' threshold>',
-                    help='FSL bet threshold used for brain masking, with '
-                    'default set to 0.25')
+                    help='FSL bet threshold used for brain masking. '
+                    'Default: 0.25')
 parser.add_argument('--fit_constraints', default='0,1,0',
                     help='Constrain the WLLS fit. '
                     'Default: 0,1,0.')
@@ -471,10 +471,12 @@ if args.mask:
     brainmask_fsl_name = 'brain'
     brainmask_fsl_full = op.join(outpath, brainmask_fsl_name)
     brainmask_fsl_out = op.join(outpath, brainmask_fsl_name + '_mask' +
-    fsl_suffix)
+    '.nii' + fsl_suffix)
     brainmask_out = op.join(outpath, brainmask_fsl_name + '_mask' + '.nii')
     B0_name = 'B0.nii'
+    B0_mean = 'B0_mean.nii'
     B0_full = op.join(outpath, B0_name)
+    B0_mean_full = op.join(outpath, B0_mean)
     # check to see if this already exists
     if op.exists(brainmask_out):
         if not (args.resume or args.force):
@@ -482,25 +484,25 @@ if args.mask:
                             'In order to run please delete the files, use '
                             '--force, use --resume, or change output '
                             'destination.')
-    if args.maskthresh is None:
-        maskthresh = 0.25
+    if args.maskthr is None:
+        maskthr = 0.25
     else:
-        maskthresh = args.maskthresh
-    # Mask based on last preprocessing step; get filename
-    last_fname = filetable['HEAD'].getName() + '.nii'
+        maskthr = args.maskthr
     # Extract B0s
+    print(filetable['HEAD'].getFull())
     executeThis = ['dwiextract', '-force', '-fslgrad',
                    filetable['dwi'].getBVEC(), filetable['dwi'].getBVAL(),
                    '-bzero', filetable['HEAD'].getFull(), B0_full]
+    print(executeThis)
     completion = subprocess.run(executeThis)
     # Compute mean B0s
     executeThis = ['mrmath', '-force', '-axis', '3', B0_full, 'mean',
-                   B0_full]
+                   B0_mean_full]
     completion = subprocess.run(executeThis)
     if completion.returncode != 0:
         raise Exception('B0 extraction failed: check your .bval file')
     # Remove NaNs
-    executeThis = ['fslmaths', B0_full, '-nan', B0_full + fsl_suffix]
+    executeThis = ['fslmaths', B0_mean_full, '-nan', B0_full + fsl_suffix]
     completion = subprocess.run(executeThis)
     if completion.returncode != 0:
         raise Exception('Unable to remove NaNs from B0.nii. '
@@ -514,10 +516,14 @@ if args.mask:
     if os.path.exists(B0_full + fsl_suffix):
         os.remove(B0_full + fsl_suffix)
     executeThis = ['bet', B0_full, brainmask_fsl_full, '-m', '-f',
-                   np.str(maskthresh)]
+                   np.str(maskthr)]
+    print(executeThis)
     completion = subprocess.run(executeThis)
     if completion.returncode != 0:
-        raise Exception('Brain extraction failed.')
+        raise Exception('Brain extraction failed. Check your B0.nii file '
+                        'to verify correct extraction, then run with '
+                        '--resume flag to continue preprocessing from '
+                        'here.')
     # Decompress fsl's gunzip format
     with gzip.open(brainmask_fsl_out, 'r') as f_in, \
             open(brainmask_out,'wb') as f_out: