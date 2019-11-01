Fix IAS tortuosity calculation

designer/fitting/dwipi.py
@@ -823,7 +823,7 @@ class DWI(object):
                 eigval = np.sort(eigval)[::-1]
                 ias_ad = eigval[0]
                 ias_rd = 0.5 * (eigval[1] + eigval[2])
-                ias_tort = eas_ad / eas_rd
+                ias_tort = ias_ad / ias_rd
             except:
                 ias_ad = 0
                 ias_rd = 0