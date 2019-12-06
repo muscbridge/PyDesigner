b43b9c7c399b873144b363d3fd5e58ee63b1420c
Siddhartha Dhiman <sdhiman@buffalo.edu>
2019-12-06 17:52:00 -0500

    Refine class init

--- designer/preprocessing/snrplot.py
+++ designer/preprocessing/snrplot.py
@@ -77,22 +77,22 @@ class makesnr:
             raise Exception('Please provide the path to noise map from '
                             '"dwidenoise"')
 
-        nDWI = len(dwilist)     # Number of input DWIs
+        self.nDWI = len(dwilist)     # Number of input DWIs
         # Open the first image in list
-        hdr = nib.load(dwilist[0])
-        sDWI = hdr.shape        # Shape of input DWIs
-        nvox = sDWI[0] * sDWI[1] * sDWI[2]
-        if hdr.ndim != 4:
+        self.hdr = nib.load(dwilist[0])
+        sDWI = self.hdr.shape        # Shape of input DWIs
+        self.nvox = sDWI[0] * sDWI[1] * sDWI[2]
+        if self.hdr.ndim != 4:
             raise IOError('Input DWIs need are not 4D. Please ensure you '
                           'use 4D NifTi files only.')
             print('No brain mask supplied')
         # Load image into 2D array
-        self.img = np.array(hdr.dataobj)
+        self.img = np.array(self.hdr.dataobj)
         # Load noise into a vector
         self.noise = np.array(nib.load(noisepath).dataobj)
         # Load BVAL
         BVALpath = util.DWIFile(dwilist[0]).getBVAL()
-        self.bval = np.rint(np.loadtxt(BVALpath) / 1000)
+        self.bval = np.rint(np.loadtxt(BVALpath) / 1000).reshape(-1)
         if maskPath is not None and op.exists(maskPath):
             self.mask = np.array(nib.load(maskPath).dataobj).astype(bool)
             self.maskStatus = True
@@ -104,24 +104,25 @@ class makesnr:
         self.img = vectorize(self.img, self.mask)
         print(self.img.shape)
         self.noise = vectorize(self.noise, self.mask)
-        # From second image to last image
-        for i in range(1, nDWI):
-            try:
-                tmp = vectorize(np.array(nib.load(dwilist[i]).dataobj),
-                                self.mask)
-                print(tmp.shape)
-                self.img = np.dstack((self.img, tmp))
-            except:
-                raise ValueError('all input DWIs must have the same '
-                                 'shape.')
-            try:
-                self.bval = np.stack((self.bval,
-                    np.rint(
-                        np.loadtxt(
-                            util.DWIFile(dwilist[i]).getBVAL()) / 1000)))
-            except:
-                raise IOError('Unable to locate BVAL file for image: {'
-                              '}'.format(dwilist[i]))
+        if self.nDWI > 1:
+            # From second image to last image
+            for i in range(1, self.nDWI):
+                try:
+                    tmp = vectorize(np.array(nib.load(dwilist[i]).dataobj),
+                                    self.mask)
+                    print(tmp.shape)
+                    self.img = np.dstack((self.img, tmp))
+                except:
+                    raise ValueError('all input DWIs must have the same '
+                                     'shape.')
+                try:
+                    self.bval = np.stack((self.bval,
+                        np.rint(
+                            np.loadtxt(
+                                util.DWIFile(dwilist[i]).getBVAL()) / 1000)))
+                except:
+                    raise IOError('Unable to locate BVAL file for image: {'
+                                  '}'.format(dwilist[i]))
 
     def computesnr(self):
         """
