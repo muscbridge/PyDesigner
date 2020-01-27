f1ac14932599629fa2a69078c3e6cde5ce4fb097
Siddhartha Dhiman <sdhiman@buffalo.edu>
2020-01-27 12:09:04 -0500

    Add __init__.py for postprocessing modules

---
+++ designer/postprocessing/medianfilter.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding : utf-8 -*-
+
+"""
+Median filter module written specifically for PyDesigner. This method
+uses nothing other than NumPy to keep dependencies low.
+"""
