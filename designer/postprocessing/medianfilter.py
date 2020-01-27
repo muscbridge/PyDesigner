4228d09981eba64702dd575b14d1cdf0bd6ad585
Siddhartha Dhiman <sdhiman@buffalo.edu>
2020-01-27 12:09:04 -0500

    Revert "Add __init__.py for postprocessing modules"
    This reverts commit f1ac14932599629fa2a69078c3e6cde5ce4fb097.

--- designer/postprocessing/medianfilter.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding : utf-8 -*-
-
-"""
-Median filter module written specifically for PyDesigner. This method
-uses nothing other than NumPy to keep dependencies low.
-"""
