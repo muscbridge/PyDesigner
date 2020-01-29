cedffd7435c432e9c3837c03aec520068c87123b
Siddhartha Dhiman <dhiman@musc.edu>
2020-01-29 13:57:02 -0500

    Add undistort function

--- designer/preprocessing/mrpreproc.py
+++ designer/preprocessing/mrpreproc.py
@@ -4,8 +4,10 @@
 """
 Utilities for running various MRtrix3's DWI preprocessing tools
 """
+import os
 import os.path as op
 import subprocess
+from designer.preprocessing import preparation, util
 
 def miftonii(input, output, strides='1,2,3,4', nthreads=None,
              force=True, verbose=False):
@@ -172,9 +174,93 @@ def degibbs(input, output, nthreads=None, force=False, verbose=False):
         raise Exception('mrdegibbs failed, please look above for error '
                         'sources.')
 
+def undistort(input, output, rpe='rpe_header', qc=None, 
+              nthreads=None, force=False, verbose=False):
+    """
+    Runs MRtrix3's `dwipreproc` command with optimal parameters for
+    PyDesigner.
 
+    Parameters
+    ----------
+    input (str):      path to input .mif file
 
+    output (str):     path to output .mif file
+    nthreads (int):   number of threads in multi-threaded applications
+    force (bool):     force overwrite of output files (default: False)
+    verbose (bool):   display information messages (default: False)
 
-
-
-
+    Returns
+    -------
+    system call:    (none)
+    """
+    if not op.exists(input):
+        raise OSError('Input path does not exist. Please ensure that '
+                      'the folder or file specified exists.')
+    if not op.exists(op.dirname(output)):
+        raise OSError('Specifed directory for output file {} does not '
+                      'exist. Please ensure that this is a valid '
+                      'directory.'.format(op.dirname(output)))
+    if not rpe in ['rpe_none', 'rpe_pair', 'rpe_all', 'rpe_header']:
+        raise Exception('Entered RPE selection is not valid. Please '
+                        'choose either "rpe_none", "rpe_pair", '
+                        '"rpe_all", or "rpe_header".')
+    if not qc is None:
+        if not isinstance(qc, str):
+            raise Exception('Please specify QC directory as a string')
+        if not op.exists(qc):
+            raise OSError('Specified QC directory does not exist. '
+                          'Please ensure that this is a valid '
+                          'directory.')
+    if not (nthreads is None):
+        if not isinstance(nthreads, int):
+            raise Exception('Please specify the number of threads as an '
+                            'integer.')
+    if not isinstance(force, bool):
+        raise Exception('Please specify whether forced overwrite is True '
+                        'or False.')
+    if not isinstance(verbose, bool):
+        raise Exception('Please specify whether verbose is True or False.')
+    
+    rpe = '-' + rpe
+    # Get output directory
+    outdir = op.dirname(output)
+    # Extract BVEC and BVALS for shell sampling deduction
+    arg_extract = ['mrinfo']
+    arg_extract.extend(['-export_grad_fsl',
+                        op.join(outdir, 'dwiec.bvec'),
+                        op.join(outdir, 'dwiec.bval')])
+    arg_extract.append(input)
+    completion = subprocess.run(arg_extract)
+    if completion.returncode != 0:
+        raise Exception('extracting FSL BVEC and BVEC gradients '
+                        'failed during undistortion, please look '
+                        'above for errors.')
+    # Form main undistortion argument
+    arg = ['dwipreproc']
+    if force:
+        arg.append('-force')
+    if not verbose:
+        arg.append('-quiet')
+    if not (nthreads is None):
+        arg.extend(['-nthreads', nthreads])
+    # Determine whether half or full sphere sampling
+    repol_string = '--repol '
+    if util.bvec_is_fullsphere(op.join(outdir, 'dwiec.bvec')):
+        # is full, add appropriate dwipreproc option
+        repol_string += '--data_is_shelled'
+    else:
+        # half
+        repol_string += '--slm=linear'
+    arg.extend(['-eddy_options', repol_string])
+    arg.append(rpe)
+    if not qc is None:
+        arg.extend(['-eddyqc_all', qc])
+    arg.extend([input, output])
+    completion = subprocess.run(arg, cwd=outdir)
+    if completion.returncode != 0:
+        raise Exception('dwipreproc failed, please look above for '
+                        'error sources.')
+    # Remove temporarily generated files
+    os.remove(op.join(outdir, 'dwiec.bvec'))
+    os.remove(op.join(outdir, 'dwiec.bval'))
+    
