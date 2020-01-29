04dffe4c4bfa10f9d0c48ed6566035234ff74b94
Siddhartha Dhiman <sdhiman@buffalo.edu>
2020-01-28 19:43:39 -0500

    Init `mrpreproc` module
    Makes system calls to MRtrix3's DWI preprocessing commands

---
+++ designer/preprocessing/mrpreproc.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# -*- coding : utf-8 -*-
+
+"""
+Utilities for running various MRtrix3's DWI preprocessing tools
+"""
+import os.path as op
+import subprocess
+
+def miftonii(input, output, strides='1,2,3,4', nthreads=None,
+             force=True, verbose=False):
+    """
+    Converts input `.mif` images to output `.nii` images
+
+    Parameters
+    ----------
+    input (str):    path to input .mif file
+    output (str):   path to output .nii file
+    strides (str):  specify the strides of the output data in memory
+                    (default: '1,2,3,4')
+
+    Returns
+    -------
+    (none)
+    """
+    if not op.exists(input):
+        raise OSError('Input path does not exist. Please ensure that '
+                      'the folder or file specified exists.')
+    if not op.isdir(op.dirname(output)):
+        raise OSError('Specifed directory for output file {} does not '
+                      'exist. Please ensure that this is a valid '
+                      'directory.'.format(op.dirname(input)))
+    if not isinstance(strides, str):
+        raise Exception('Please specify strides as a string.')
+    if not (nthreads is None):
+        if not isinstance(nthreads, int):
+            raise Exception('Please specify the number of threads as an '
+                            'integer.')
+    if not isinstance(force, bool):
+        raise Exception('Please specify whether forced overwrite is True '
+                        'or False.')
+    if not isinstance(verbose, bool):
+        raise Exception('Please specify whether verbose is True or False.')
+    arg = ['mrconvert']
+    if force:
+        arg.append('-force')
+    if not verbose:
+        arg.append('-quiet')
+    if not (nthreads is None):
+        arg.extend(['-nthreads', nthreads])
+    arg.extend(['-export_grad_fsl',
+                op.splitext(output)[0] + '.bvec',
+                op.splitext(output)[0] + '.bval'])
+    arg.extend(['-json_export', op.splitext(output)[0] + '.json'])
+    arg.extend(['-strides', strides])
+    arg.extend([input, output])
+    completion = subprocess.run(arg)
+    if completion.returncode != 0:
+        raise Exception('Conversion from .mif to .nii failed; check '
+                        'above for errors.')
+
+def denoise(input, output, noisemap=True, extent='5,5,5', nthreads=None,
+            force=True, verbose=False):
+    """
+    Runs MRtrix3's `dwidenoise` command with optimal parameters for
+    PyDesigner.
+
+    Parameters
+    ----------
+    input (str):      path to input .mif file
+
+    output (str):     path to output .mif file
+    noisemap (bool):  specify whether or not to save the noisemap as a
+                      nifti file (default: True)
+    extent (str):     set the window size of the denoising filter.
+                      (default: 5,5,5)
+    nthreads (int):   number of threads in multi-threaded applications
+    force (bool):     force overwrite of output files (default: False)
+    verbose (bool):   display information messages (default: False)
+
+    Returns
+    -------
+    none
+    """
+    if not op.exists(input):
+        raise OSError('Input path does not exist. Please ensure that '
+                      'the folder or file specified exists.')
+    if not op.isdir(op.dirname(output)):
+        raise OSError('Specifed directory for output file {} does not '
+                      'exist. Please ensure that this is a valid '
+                      'directory.'.format(op.dirname(input)))
+    if not isinstance(noisemap, bool):
+        raise Exception('Please specify whether noisemap generation '
+                        'is True or False.')
+    if not isinstance(extent, str):
+        raise Exception('Please specify extent as a string formatted as '
+                        '"n,n,n".')
+    if not (nthreads is None):
+        if not isinstance(nthreads, int):
+            raise Exception('Please specify the number of threads as an '
+                            'integer.')
+    if not isinstance(force, bool):
+        raise Exception('Please specify whether forced overwrite is True '
+                        'or False.')
+    if not isinstance(verbose, bool):
+        raise Exception('Please specify whether verbose is True or False.')
+    noisemap_path = op.join(op.dirname(input), 'noisemap.nii')
+    arg = ['dwidenoise']
+    if force:
+        arg.append('-force')
+    if not verbose:
+        arg.append('-quiet')
+    if not (nthreads is None):
+        arg.extend(['-nthreads', nthreads])
+    if noisemap:
+        arg.extend(['-noise', noisemap_path])
+    if not (extent is None):
+        arg.extend(['-extent', extent])
+    arg.extend([input, output])
+    completion = subprocess.run(arg)
+    if completion.returncode != 0:
+        raise Exception('dwidenoise failed, please look above for error '
+                        'sources.')
+
+
+
+
+
