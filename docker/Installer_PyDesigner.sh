d65e55a7c99357147e88ac09733f18924eae732e
Siddhartha Dhiman <dhiman@musc.edu>
2020-02-18 14:01:21 -0500

    Update all Docker scripts (#161)

--- docker/Installer_PyDesigner.sh
+++ docker/Installer_PyDesigner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Downloads and extracts PyDesigner
-mkdir -p /usr/local/PyDesigner
+mkdir -p /tmp/PyDesigner
 export URL=$(curl -s https://api.github.com/repos/m-ama/PyDesigner/releases | jq -r '.[0] | .tarball_url')
-wget -q -O - $URL | tar -xzvf - -C /usr/local/PyDesigner --strip 1
+wget -q -O - $URL | tar -xzvf - -C /tmp/PyDesigner --strip 1
