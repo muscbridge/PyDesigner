acdd6f2f08a575926840351d21f68532b654958a
Siddhartha Dhiman <sid.kill3r@gmail.com>
2020-07-15 18:06:25 -0400

    Update list of thresholds

--- designer/fitting/thresholds.py
+++ designer/fitting/thresholds.py
@@ -1,2 +1,6 @@
-__maxdkibval__ = 2.5
-__minZero__ = 1e-8
+__maxdkibval__ = 2.5    # minimum DTI B-value
+__mindkibval__ = 0.5    # maximum DKI B-value
+__minfbibval__ = 4.0    # minimum FBI B-value
+__d0__ = 3.0
+__minZero__ = 1e-8      # threshold under which all numbers are zero
+__dirs__ = 256          # Define number of directions to resample after computing all tensors 
