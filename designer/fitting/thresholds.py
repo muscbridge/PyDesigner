f1900c76960baa468244fe878c2ca47c94b1d183
Siddhartha Dhiman <sid.kill3r@gmail.com>
2020-08-03 18:28:57 -0400

    Change minimum zero to 10E-8

--- designer/fitting/thresholds.py
+++ designer/fitting/thresholds.py
@@ -6,5 +6,5 @@ __minfbibval__ = 4.0    # minimum FBI B-value
 __d0__ = 3.0            # diffusivity of free water at body temp (37 deg C) for upper bound on Da
 __dn__ = 1.5            # estimated intra-neurite diffusivity in mm^2/ms
 __pkT__ = 0.4           # peak thresholding for white matter fiber tracking
-__minZero__ = 1e-8      # threshold under which all numbers are zero
+__minZero__ = 10e-8      # threshold under which all numbers are zero
 __dirs__ = 256          # Define number of directions to resample after computing all tensors 
