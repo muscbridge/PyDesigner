2b3ca8ef068c997ebeacd94b107ba21da63270b1
Siddhartha Dhiman <sid.kill3r@gmail.com>
2024-09-16 23:45:36 -0400

    Add distributed unit testing - xdist

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -3,7 +3,7 @@
 # If no arguments are passed, run pytest with coverage
 if [ "$#" -eq 0 ]; then
     mkdir -p /test_results
-    pytest tests -vv  --cov=pydesigner --cov-report=xml:/test_results/coverage.xml --junitxml=/test_results/results.xml
+    pytest tests -vv  -n auto --cov=pydesigner --cov-report=xml:/test_results/coverage.xml --junitxml=/test_results/results.xml
 else
     # Otherwise, run the command passed as arguments
     exec "$@"
